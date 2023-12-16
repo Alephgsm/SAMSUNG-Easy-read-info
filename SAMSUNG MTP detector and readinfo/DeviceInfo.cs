083c1b187b5976906558fee99c43cb5d7fcdcf16
AsgharSo <AsgharSo@DESKTOP-H38B28I>
2023-12-16 20:13:38 +0330

    Add project files.

---
+++ SAMSUNG MTP detector and readinfo/DeviceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAMSUNG_Easy_read_info
+{
+	public struct DeviceInfo
+	{
+		public string DeviceName;
+		public string Model;
+		public string Region;
+		public string PDAVersion;
+		public string CSCVersion;
+		public string MODEMVersion;
+		public string UN;
+		public string IMEI;
+		public string SN;
+	}
+}
---
+++ SAMSUNG MTP detector and readinfo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO.Ports;
+using System.Management;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace SAMSUNG_Easy_read_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			foreach (ManagementBaseObject managementBaseObject in new ManagementClass("Win32_SerialPort").GetInstances())
+			{
+				ManagementObject managementObject = (ManagementObject)managementBaseObject;
+				if (managementObject["name"].ToString().Contains("SAMSUNG Mobile USB Modem"))
+				{
+					try
+					{
+						Thread.Sleep(1000);
+						SerialPort serialPort = new SerialPort();
+						serialPort.PortName = managementObject["deviceid"].ToString();
+						serialPort.DataBits = 8;
+						serialPort.BaudRate = 19200;
+						Thread.Sleep(100);
+						if (!serialPort.IsOpen)
+						{
+							serialPort.Open();
+						}
+						Thread.Sleep(100);
+						serialPort.WriteLine("AT+DEVCONINFO\r");
+						Thread.Sleep(100);
+						string text = serialPort.ReadExisting();
+						serialPort.Close();
+						if (text.Contains("BUSY"))
+						{
+							serialPort.Dispose();
+						}
+						else
+						{
+							var groups = new Regex("MN\\((.*?)\\);BASE\\((.*?)\\);VER\\((.*?)/(.*?)/(.*?)/(.*?)\\);HIDVER\\(.*?\\);MNC\\(.*?\\);MCC\\(.*?\\);PRD\\((.*?)\\);.*?SN\\((.*?)\\);IMEI\\((.*?)\\);UN\\((.*?)\\);").Match(text).Groups;
+							if (groups.Count > 1)
+							{
+								var dev = new DeviceInfo
+								{
+									Model = groups[1].Value,
+									DeviceName = groups[1].Value,
+									PDAVersion = groups[3].Value,
+									CSCVersion = groups[4].Value,
+									MODEMVersion = groups[5].Value,
+									Region = groups[7].Value.Substring(groups[7].Value.Length - 3),
+									SN = groups[8].Value,
+									IMEI = groups[9].Value,
+									UN = groups[10].Value
+								};
+								Console.WriteLine("Model : " + dev.Model);
+								Console.WriteLine("DeviceName : " + dev.DeviceName);
+								Console.WriteLine("PDA Version : " + dev.PDAVersion);
+								Console.WriteLine("CSC Version : " + dev.CSCVersion);
+								Console.WriteLine("MODEM Version : " + dev.MODEMVersion);
+								Console.WriteLine("Region : " + dev.Region);
+								Console.WriteLine("SN : " + dev.SN);
+								Console.WriteLine("IMEI : " + dev.IMEI);
+								Console.WriteLine("UN : " + dev.UN);
+							}
+						}
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+			}
+		}
+	}
+}
